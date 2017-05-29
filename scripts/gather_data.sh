fabb589f01f301b610465eea1dd8176d13de4816
Maxwell Horton <maxwell.christian.horton@gmail.com>
2017-05-29 13:26:24 -0700

    Added script to measure GPU watt usage
    Requires nvidia-smi command.

---
+++ scripts/gather_data.sh
@@ -0,0 +1,24 @@
+# Gather timestamped GPU power usage data and store it in a text file.
+#!/bin/sh
+
+time_millis() {
+  date +%s%N | cut -b1-13
+}
+
+get_all_watts() {
+  nvidia-smi | sed -n "s:.*\(...\)W /.*:\1:p" | awk '{print $1}' | paste -s -d, -
+}
+
+_REC_TIME=10000 # run for 10k milliseconds, or 10 seconds
+INTERVAL=.1 # Gather data every 0.1s
+
+START=$(time_millis)
+END=$(($START + $_REC_TIME))
+_TIME=$(time_millis)
+
+while [ $_TIME -lt $END ]
+do
+  _TIME=$(time_millis)
+  echo $_TIME,$(get_all_watts)
+  sleep $INTERVAL
+done
