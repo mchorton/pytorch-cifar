c668d7319a50312ce22825daf13c3778984e4bc5
Maxwell Forbes <mbforbes@cs.washington.edu>
2017-06-05 15:49:06 -0700

    script time -> 60s

--- scripts/gather_data.sh
+++ scripts/gather_data.sh
@@ -9,8 +9,8 @@ get_all_watts() {
   nvidia-smi | sed -n "s:.*\(...\)W /.*:\1:p" | awk '{print $1}' | paste -s -d, -
 }
 
-_REC_TIME=30000 # run for 10k milliseconds, or 10 seconds
-INTERVAL=.1 # Gather data every 0.1s
+_REC_TIME=60000  # 10000 = run for 10k milliseconds, or 10 seconds
+INTERVAL=.1  # Gather data every 0.1s
 
 START=$(time_millis)
 END=$(($START + $_REC_TIME))
